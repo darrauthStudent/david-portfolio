93dc556f99d0afd92377d0371628786edf8a4fc8
Sebastian Alejandro Asprino Ortiz <sebastian@asprino.dev>
2026-02-02 13:24:27 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
   vite: {
-      // @ts-ignore
-      plugins: [tailwindcss()]
+    plugins: [tailwindcss()]
   },
 
   output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+
   integrations: [react()],
-  adapter: cloudflare(),
-}); 
+});
